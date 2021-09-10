b06bee483f8dc95a2a7342b90c1f2a81bae1e0ec
oleksii <oleksii.latukhov@dataart.com>
2021-09-10 19:03:26 +0300

    preparation for API

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object {
+        const val API_KEY = "ca3bc72fe3da446cb74b1752bb0dbe22"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
