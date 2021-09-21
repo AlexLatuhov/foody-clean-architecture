143f3eb580121b1d33aa8fdbe21cebdc7fac548a
oleksii <oleksii.latukhov@dataart.com>
2021-09-21 14:26:20 +0300

    details screen with tabs

--- app/src/main/java/com/example/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/foody/ui/DetailsActivity.kt
@@ -4,16 +4,45 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.example.foody.R
+import com.example.foody.adapters.PagerAdapter
 import com.example.foody.databinding.ActivityDetailsBinding
+import com.example.foody.ui.fragments.ingredients.IngredientsFragment
+import com.example.foody.ui.fragments.instructions.InstructionsFragment
+import com.example.foody.ui.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        binding.viewPager.adapter =
+            PagerAdapter(resultBundle, fragments, supportFragmentManager, lifecycle)
+
+        val titles = ArrayList<Int>()
+        titles.add(R.drawable.ic_overview)
+        titles.add(R.drawable.ic_ingredients)
+        titles.add(R.drawable.ic_instructions)
+        //todo make it work with title text
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, pos ->
+            tab.icon = ContextCompat.getDrawable(this, titles[pos])
+        }.attach()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
