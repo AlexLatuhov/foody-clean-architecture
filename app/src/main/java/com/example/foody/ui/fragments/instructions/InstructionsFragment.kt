720f6f899b18c9416dcb858315148415cef9f5ec
oleksii <oleksii.latukhov@dataart.com>
2021-09-22 12:33:44 +0300

    instructions screen

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.foody.R
+import com.example.foody.models.Result
+import com.example.foody.util.Constants
 
 class InstructionsFragment : Fragment() {
 
@@ -13,6 +17,13 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE)
+        val webView = view.findViewById<WebView>(R.id.instructions_webView)
+        webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        webView.loadUrl(websiteUrl)
+        return view
     }
 }
